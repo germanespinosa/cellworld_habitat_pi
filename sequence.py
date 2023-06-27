333799695546346b5df5b754aca9bc39820cfc9a
AlexT <alexanderlai2024@u.northwestern.edu>
2023-06-27 14:46:45 -0500

    changed sequence to length 1

--- sequence.py
+++ sequence.py
@@ -5,7 +5,7 @@ from json_cpp import JsonList
 
 class Sequence:
     def rand_no_consec_rep(self, reward_cells):
-        seq_len = 10
+        seq_len = 1
         reward_sequence = [0 for s in range(seq_len)]
         cell = random.choice(list(reward_cells))
         reward_sequence[0] = cell
@@ -16,12 +16,3 @@ class Sequence:
             cell = random.choice(non_consec_list)
             reward_sequence[i+1] = cell
         return Cell_group_builder(JsonList(reward_sequence))
-
-
-
-
-reward_cells = Cell_group_builder(JsonList([0,155,330,240]))
-print(reward_cells)
-sequence = Sequence()
-reward_sequence = sequence.rand_no_consec_rep(reward_cells)
-print(reward_sequence)
