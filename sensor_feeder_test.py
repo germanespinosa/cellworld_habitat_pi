6eb7643695e7ade674d402e425f32f3f923c4b79
chris-angeloni <chris.angeloni@gmail.com>
2023-12-01 20:30:22 +0000

    sensor_feeder_test.py update to have arguments

--- sensor_feeder_test.py
+++ sensor_feeder_test.py
@@ -2,15 +2,31 @@
 
 import RPi.GPIO as GPIO
 from time import sleep
+import sys
 
-delay = 0.1
+print(len(sys.argv))
+if len(sys.argv) > 0:
+	if len(sys.argv) == 2:
+		delay = float(sys.argv[1])
+		reps = 100
+	elif len(sys.argv) == 3:
+		delay = float(sys.argv[1])
+		reps = int(sys.argv[2])
+	else:
+		delay = 0.05
+		reps = 100
+
+
+print(f'delay = {delay}, reps = {reps}')
+
+
 input = 22
 output = 27
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(input, GPIO.IN)
 GPIO.setup(output, GPIO.OUT)
 
-for i in range(100):
+for i in range(reps):
 	GPIO.output(output, True)
 	sleep(delay)
 	GPIO.output(output, False)
